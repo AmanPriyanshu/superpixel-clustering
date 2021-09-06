ccc29829cb2e91009e2e8b965f006bde663de532
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2021-09-06 18:52:20 +0530

    recording

--- gen.py
+++ gen.py
@@ -5,7 +5,7 @@ import os
 path = './images/'
 images = np.stack([cv2.imread(path+i) for i in os.listdir(path)])
 print(images.shape)
-video = cv2.VideoWriter('record.mp4', 0, 40, (images.shape[2], images.shape[1])) 
+video = cv2.VideoWriter('record.mp4', 0, 24, (images.shape[2], images.shape[1])) 
 for image in images:
 	video.write(image) 
 cv2.destroyAllWindows() 
